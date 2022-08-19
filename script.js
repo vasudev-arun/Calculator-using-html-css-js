a0f48978e7f927bde0849a98df1c31c104613707
Arun Vasudevan <69780859+vasudev-arun@users.noreply.github.com>
2022-08-19 15:39:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,125 @@
-.calc{
-    width: 400px;
-    height: 440px;
-    background-color: rgb(2, 33, 72);
-    margin-left: 100px;
-    margin-top: 35px;
+var IsDotPressed=0;
+var operator,num1,num2,result;
+
+function buttonclick(val){
+    switch (val){
+        case 1:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 2:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 3:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 4:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 5:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 6:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 7:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 8:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 9:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case 0:{
+            document.getElementById("screen").value+=val;
+            break;
+        }
+        case ".":{
+            if(IsDotPressed==0){
+                document.getElementById("screen").value+=val;
+                IsDotPressed=1;
+            }
+            else{
+                document.getElementById("screen").value;
+            }
+            
+            break;
+        }
+        case "/":{
+            num1=document.getElementById("screen").value;
+            cleardisplay();
+            IsDotPressed=0;
+            operator=1;
+            break;
+        }
+        case "*":{
+            num1=document.getElementById("screen").value;
+            cleardisplay();
+            IsDotPressed=0;
+            operator=2;
+            break;
+        }
+        case "-":{
+            num1=document.getElementById("screen").value;
+            cleardisplay();
+            IsDotPressed=0;
+            operator=3;
+            break;
+        }
+        case "+":{
+            num1=document.getElementById("screen").value;
+            cleardisplay();
+            IsDotPressed=0;
+            operator=4;
+            break;
+        }
+        case "=":{
+            num2=document.getElementById("screen").value;
+            IsDotPressed=0;
+            switch(operator){
+                case 1:{
+                    result=num1/num2;
+                    document.getElementById("screen").value=result;
+                    break;
+                }
+                case 2:{
+                    result=num1*num2;
+                    document.getElementById("screen").value=result;
+                    break;
+                }
+                case 3:{
+                    result=num1-num2;
+                    document.getElementById("screen").value=result;
+                    break;
+                }
+                case 4:{
+                    result= parseFloat(num1)+ parseFloat(num2);
+                    document.getElementById("screen").value=result;
+                    break;
+                }
+            }
+
+            break;
+        }
+
+    }
     
+
 }
-#screen {
-    width: 80%;
-    margin-left: 37px;
-    height: 60px;
-    margin-top: 20px;
-    pointer-events: none;
-    font-size: 50px;
-    text-align: right;
-    background-color: rgb(169, 246, 92);
-    font-family: Lato,Helvetica,Arial,sans-serif;
+function cleardisplay(){
+    document.getElementById("screen").value="";
+    IsDotPressed=0;
 }
-.btn {
-    height: 50px;
-    width: 60px;
-    margin-left: 30px;
-    margin-top: 10px;
-    background-color: rgb(55, 96, 173);
-    color: cornsilk;
-    font-size: larger;
-    border-radius: 30%;
-}
-.heading{
-    padding-left: 150px;
-    padding-top: 15px;
-    color: rgb(255, 255, 255);
-    text-decoration:wavy;
-    text-orientation: sideways;
-    font-size: larger;
-    font-weight: bold;
-    
+function backspace(){
+    document.getElementById("screen").value=document.getElementById("screen").value.toString().split('').slice(0, -1).join('')
+
 }
